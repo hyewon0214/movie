39e5d71bbbafed4c8d45f1f5ff49479b2d38a532
01-ksh <ksh7627124@naver.com>
2021-05-19 20:25:55 +0900

    student2

---
+++ movie_score.py
@@ -0,0 +1,7 @@
+    result.append([Title, rating, aud])
+    time.sleep(3)
+    search_box = driver.find_element_by_name('query')
+    search_box.clear()
+time.sleep(3)
+print(result)
+driver.close()
